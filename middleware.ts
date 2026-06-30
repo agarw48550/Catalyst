b62c42e9b23b6d5af2bdca2a29344c54842cc1e9
Ayaan <agarw48550@gapps.uwcsea.edu.sg>
2026-06-30 15:35:01 +0800

    Update middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -10,15 +10,12 @@ const isProtectedRoute = createRouteMatcher([
 ])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect()
-  }
+  if (isProtectedRoute(req)) await auth.protect()
 })
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
-    '/__clerk/:path*',
   ],
 }
